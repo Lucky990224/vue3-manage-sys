4d362e389942a4ddfff8e4767f4ea536219dd419
yalo <yalo.wang@flatek.com>
2024-01-31 10:16:30 +0800

    优化main.js

--- src/main.js
+++ src/main.js
@@ -4,8 +4,7 @@ import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
 
-const pinia = createPinia()
 const app = createApp(App)  // 创建根组件
 app.use(router)             // 注册路由
-app.use(pinia)              
+app.use(createPinia())              
 app.mount('#app')  
